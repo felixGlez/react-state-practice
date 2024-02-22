8a71c48dc74d30049be790d29f0d11e96d5b0e9d
felixGlez <felixgonzalezgarcia2010@gmail.com>
2024-02-22 16:46:57 +0100

    Update Square.jsx

--- src/components/square/Square.jsx
+++ src/components/square/Square.jsx
@@ -3,6 +3,25 @@ import Button from '../button/Button';
 import { StyledSquare } from './styles';
 
 const Square = () => {
+	/*
+    IMPORTANTE: Los estados que sean siempre que se pueda o Booleanos o Números.
+    Con booleano (que sería la forma más correcta):
+
+    const [isGreen, setIsGreen] = useState(false)
+
+    return (
+		<StyledSquare $color={isGreen ? 'green' : 'red'}>
+			<Button action={() => changeColor(isGreen, setIsGreen)}>Change Color</Button>
+		</StyledSquare>
+	);
+
+    -------La función----------
+    
+    const changeColor = (isGreen, setIsGreen) => {
+        setIsGreen(!isGreen)
+    };
+
+    */
 	const [color, setColor] = useState('red');
 	console.log(color);
 	return (
